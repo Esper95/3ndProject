64b645ac238241bd891cf2218465eb1faa8900d1
Epser95 <angeicjs95@github.com>
2021-02-15 00:51:55 +0900

    로그인 구현 끝

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
-$(document).ready(function($) {
-    var $form_modal = $('.user-modal'),
-        $form_login = $form_modal.find('#login'),
-        $form_signup = $form_modal.find('#')
+var modal;
+// Get the modal
+$('.login_page').on('click', function(event) {
+    modal = document.getElementById("login");
+    modal.style.display = "block";
+});
+$('.join_page').on('click', function(event) {
+    modal = document.getElementById("join");
+    modal.style.display = "block";
+});
+$('.login_page1').on('click', function(event) {
+    modal.style.display = "none";
+    modal = document.getElementById("login");
+    modal.style.display = "block";
+});
+$('.join_page1').on('click', function(event) {
+    modal.style.display = "none";
+    modal = document.getElementById("join");
+    modal.style.display = "block";
 });
 
-
-
-// Get the modal
-var modal = document.getElementById("login");
-
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span1 = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+/* btn.onclick = function() {
+    var modal = document.getElementById("login");
+} */
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+    modal.style.display = "none";
+}
+span1.onclick = function() {
     modal.style.display = "none";
 }
 
@@ -31,7 +45,6 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-
 
 
 
